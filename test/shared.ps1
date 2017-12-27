d766af6a337513f4ccbde76b033d7c64d817a12a
Hallvard Vasstveit <hallvard@vasstveit.com>
2017-12-27 20:19:09 +0100

    Shared script for tests, imports all functions, testModuleConfig

---
+++ test/shared.ps1
@@ -0,0 +1,36 @@
+# Load all functions
+$private = Get-ChildItem -Path "$PSScriptRoot\..\src\private" -Filter '*.ps1'
+$public = Get-ChildItem -Path "$PSScriptRoot\..\src\public" -Filter '*.ps1'
+
+foreach ($func in @($private + $public))
+{
+    . $func.FullName # Import to session
+}
+
+$TestModuleConfig = @{
+    Paths         = @{
+        Base       = "TestDrive:\HVM"
+        Incoming   = @{
+            Base = "Input"
+            VMs  = "VM\"
+        }
+        Enqueued   = @{
+            Base     = "ReadOnly\Enqueued"
+            ToCreate = "New\"
+            ToChange = "Set\"
+        }
+        Processing = @{
+            Base     = "ReadOnly\Processing"
+            ToCreate = "New\"
+            ToChange = "Set\"
+        }
+        Task       = "ReadOnly\Tasks\"
+        Error      = "ReadOnly\Error\"
+    }
+
+    HyperVStorage = @{
+        VMFolder  = ''
+        VhdFolder = ''
+        VhdDisks  = @($env:SystemDrive[0])
+    }
+}
