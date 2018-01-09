2ea655d74355cb02ada0bf210c186b9592cdd83b
Hallvard Vasstveit <hallvard@vasstveit.com>
2018-01-09 20:51:06 +0100

    Added name check, template check, template import is broken

--- src/public/Import-HvVmOrder.ps1
+++ src/public/Import-HvVmOrder.ps1
@@ -9,17 +9,21 @@ function Import-HvVmOrder
     begin
     {
         $_GuidRegex = '[0-9,a-f,A-F]{8}-[0-9,a-f,A-F]{4}-[0-9,a-f,A-F]{4}-[0-9,a-f,A-F]{4}-[0-9,a-f,A-F]{12}'
-
+    }
+    
+    process
+    {
         # Paths
         $incomingDir = Get-ConfigPath -Key 'Incoming', 'VMs' -ModuleConfig $ModuleConfig
         $queueNewDir = Get-ConfigPath -Key 'Enqueued', 'ToCreate' -ModuleConfig $ModuleConfig
         $queueChangeDir = Get-ConfigPath -Key 'Enqueued', 'ToChange' -ModuleConfig $ModuleConfig
         $taskDir = Get-ConfigPath -Key 'Task' -ModuleConfig $ModuleConfig
         $errorDir = Get-ConfigPath -Key 'Error' -ModuleConfig $ModuleConfig
-    }
-    
-    process
-    {
+        $statusDir = Get-ConfigPath -Key 'Status' -ModuleConfig $ModuleConfig
+
+        # 
+        # 
+        # 
         
         $toProcess = @(Get-ChildItem -Path $incomingDir -Filter '*.json' -File)
 
@@ -36,7 +40,48 @@ function Import-HvVmOrder
                 continue # Item processed
             }
 
-            $json_object = Get-Content $order.FullName | ConvertFrom-Json
+            $orderStatus = @{
+                Id            = $order.BaseName
+                Error         = ''
+                CurrentStatus = ''
+                CurrentStage  = ''
+            }
+            $orderStatus | ConvertTo-Json | Out-File -FilePath "$statusDir\$($order.Name)"
+
+            # Read file
+            $orderObject = Get-Content $order.FullName | ConvertFrom-Json
+
+            # Name is mandatory mandatory
+            if ($null -eq ($orderObject.PSObject.Properties | Where-Object { $_.Name -eq 'Name' }))
+            {
+                Move-Item -Path $order.FullName -Destination "$errorDir\$($order.Name)"
+                
+                $orderStatus.Error = 'Order does not contain Name'
+                $orderStatus | ConvertTo-Json | Out-File -FilePath "$statusDir\$($order.Name)"
+                continue
+            }
+
+            # If template we need to import properties
+            if ($null -ne ($orderObject.PSObject.Properties | Where-Object { $_.Name -eq 'Template' }))
+            {
+                try
+                {
+                    $template = Get-HVTemplate -Name $orderObject.Template
+                }
+                catch
+                {
+                    Move-Item -Path $order.FullName -Destination "$errorDir\$($order.Name)"
+                    
+                    $orderStatus.Error = $_.Exception.Message
+                    $orderStatus | ConvertTo-Json | Out-File -FilePath "$statusDir\$($order.Name)"
+                    continue
+                }
+
+                foreach ($key in $template.PSObject.Properties.Name)
+                {
+                    $orderObject.$key = $template.$key
+                }
+            }
 
             # convert to hashtable
             
@@ -44,6 +89,11 @@ function Import-HvVmOrder
 
             # Validate order
             
+
+            # Queue order
+            $orderObject | ConvertTo-Json -Depth 10 | Out-File -FilePath "$queueNewDir\$($order.Name)"
+            $orderStatus.CurrentStage = 'Queued for creation'
+            $orderStatus | ConvertTo-Json | Out-File -FilePath "$statusDir\$($order.Name)"
         }
     }
     
