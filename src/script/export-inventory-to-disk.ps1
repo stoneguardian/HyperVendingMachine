61249073b3ec25fd222a8de67c08271a2af8a1db
Hallvard Vasstveit <hallvard@halv.io>
2019-06-27 19:16:31 +0200

    script to export inventory to json-files

---
+++ src/script/export-inventory-to-disk.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter()]
+    [string] $Path = 'D:\HyperVendingMachine\Inventory'
+)
+
+if (-not (Test-Path $Path))
+{
+    $null = New-Item -Path $Path -ItemType 'Directory'
+}
+
+$inventory = Get-Inventory
+
+foreach ($key in $inventory.Keys)
+{
+    $inventory.$key | 
+        ConvertTo-Json -Depth 5 |
+        Out-File -FilePath "$Path\$key.json"
+}
