4a53309cf49f4e3dad10700aa76475a36a362306
Hallvard Vasstveit <hallvard@halv.io>
2019-06-27 17:48:31 +0200

    class for creation and discovery of disks

---
+++ src/classes/VMDisk.ps1
@@ -0,0 +1,56 @@
+class VMDisk
+{
+    [ValidateNotNullOrEmpty()]
+    [string] $VMName
+
+    [string] $Path
+
+    [ValidateRange(0, 500GB)]
+    [int] $SizeInBytes = 0
+
+    [ValidateSet('Fixed', 'Dynamic')]
+    [string] $Type
+
+    [int] $SizeInBytesOnDisk = 0
+
+    VMDisk([string]$vmName, [PSCustomObject]$order)
+    {
+        $this.VMName = $vmName
+        
+        if (-not ('SizeInBytes' -in $order.PSObject.Properties.Name))
+        {
+            Write-Error -Message "SizeInBytes is a required parameter" -ErrorAction Stop
+        }
+
+        $this.SizeInBytes = $order.SizeInBytes
+
+        if ('Type' -in $order.PSObject.Properties.Name)
+        {
+            $this.Type = $order.Type
+        }
+    }
+
+    VMDisk([string]$vmName)
+    {
+        $this.VMName = $vmName
+    }
+
+    static [VMDisk[]] Discover($vmName)
+    {
+        $disks = Get-VMHardDiskDrive -VMName $vmName
+
+        $output = foreach ($disk in $disks)
+        {
+            $vhdProperties = Get-VHD -Path $disk.Path
+
+            $out = [VMDisk]::new($vmName)
+            $out.Path = $disk.Path
+            $out.SizeInBytes = $vhdProperties.Size
+            $out.Type = $vhdProperties.VhdType
+            $out.SizeInBytesOnDisk = $vhdProperties.FileSize
+            $out # Write-Output
+        }
+
+        return $output
+    }
+}
