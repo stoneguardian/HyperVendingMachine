30278456469dce6cfbde7d5c13488bcab37f1335
Hallvard Vasstveit <hallvard@halv.io>
2019-07-02 17:47:11 +0200

    split resources into multiple json-files

--- src/script/export-host_info-to-disk.ps1
+++ src/script/export-host_info-to-disk.ps1
@@ -8,7 +8,16 @@ if (-not (Test-Path $Path))
     $null = New-Item -Path $Path -ItemType 'Directory'
 }
 
-Get-HVMAvailableResources |
-ConvertTo-Json |
-Out-File -FilePath "$Path\resources.json"
+$resources = Get-HVMAvailableResources 
 
+$resources.CPU |
+ConvertTo-Json |
+Out-File -FilePath "$Path\cpu.json"
+
+$resources.Memory |
+ConvertTo-Json |
+Out-File -FilePath "$Path\memory.json"
+
+$resources.Disk |
+ConvertTo-Json |
+Out-File -FilePath "$Path\disk.json"
