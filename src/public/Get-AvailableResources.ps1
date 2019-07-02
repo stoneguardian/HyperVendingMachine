60372e1e4697fd9e9bab7d721a22d0e47c62b426
Hallvard Vasstveit <hallvard@halv.io>
2019-07-02 18:06:37 +0200

    output int not double

--- src/public/Get-AvailableResources.ps1
+++ src/public/Get-AvailableResources.ps1
@@ -26,14 +26,14 @@ function Get-AvailableResources
         $output['Memory']['Assigned'] = (Get-VM | Measure-Object -Property MemoryStartup -Sum).Sum
         $output['Memory']['Max'] = (Get-CimInstance -ClassName 'cim_PhysicalMemory' | Measure-Object -Property Capacity -Sum).Sum * $ModuleConfig.OverprovisionFactors.Memory
         $output['Memory']['Available'] = $output['Memory']['Max'] - $output['Memory']['Assigned']
-        $output['Memory']['PercentageAssigned'] = [Math]::Round(($output['Memory']['Assigned'] * 100) / $output['Memory']['Max'], 2)
+        $output['Memory']['PercentageAssigned'] = [Math]::Ceiling(($output['Memory']['Assigned'] * 100) / $output['Memory']['Max'])
         $output['Memory']['OverprovisioningFactor'] = $ModuleConfig.OverprovisionFactors.Memory
 
         $volume = Get-Volume -DriveLetter $ModuleConfig.VMStoragePath[0] # First char is driveletter
         $output['Disk']['Used'] = $volume.Size - $volume.SizeRemaining
         $output['Disk']['Max'] = $volume.Size
         $output['Disk']['Available'] = $volume.SizeRemaining
-        $output['Disk']['PercentageUsed'] = [Math]::Round(($output['Disk']['Used'] * 100) / $output['Disk']['Max'], 2)
+        $output['Disk']['PercentageUsed'] = [Math]::Ceiling(($output['Disk']['Used'] * 100) / $output['Disk']['Max'])
 
         $output # Write-Output
     }
