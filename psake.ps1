8b386fae07f60ab9059afd61204cd9e24b230060
Hallvard Vasstveit <hallvard@halv.io>
2020-01-05 15:29:09 +0100

    explisit utf8 encoding

--- psake.ps1
+++ psake.ps1
@@ -1,4 +1,4 @@
-properties {
+Properties {
     $ModuleName = 'HyperVendingMachine'
     
     $SourceDirectoryName = 'src'
@@ -9,54 +9,54 @@ properties {
     $PSM1Path = "$ReleasePath/$ModuleName.psm1"
 }
 
-task Clean {
+Task Clean {
     if (Test-Path $ReleasePath)
     {
         $null = Remove-Item -Path $ReleasePath -Force -Recurse
     }
 }
 
-task Init -depends 'Clean'{
+Task Init -depends 'Clean' {
     $null = New-Item -Path $ReleasePath -Force -ItemType 'Directory'
 }
 
-task CopyItems -depends 'Init' {
+Task CopyItems -depends 'Init' {
     Copy-Item -Path "$SourcePath/$ModuleName.psd1" -Destination $ReleasePath
     Copy-Item -Path "$SourcePath/Configuration.psd1" -Destination $ReleasePath
     Copy-Item -Path "$SourcePath/config" -Destination $ReleasePath
     Copy-Item -Path "$SourcePath/templates" -Destination $ReleasePath -Recurse
 }
 
-task BuildPSD1 -depends 'Init' {
-    "# `n# Commit: $(git rev-parse HEAD) " | Out-File -FilePath $PSM1Path
-    "# File generated: $(Get-Date -Format u) `n#" | Out-File -FilePath $PSM1Path -Append
+Task BuildPSD1 -depends 'Init' {
+    "# `n# Commit: $(git rev-parse HEAD) " | Out-File -FilePath $PSM1Path -Encoding utf8
+    "# File generated: $(Get-Date -Format u) `n#" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
     
     if (Test-Path "$SourcePath/classes")
     {
-        "`n# Classes" | Out-File -FilePath $PSM1Path -Append
+        "`n# Classes" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
         foreach ($class in (Get-ChildItem -Path "$SourcePath/classes" -Filter '*.ps1'))
         {
-            "## $SourceDirectoryName/classes/$($class.Name)" | Out-File -FilePath $PSM1Path -Append
+            "## $SourceDirectoryName/classes/$($class.Name)" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            Get-Content -Path $class.FullName | Out-File -FilePath $PSM1Path -Append
+            Get-Content -Path $class.FullName -Encoding Utf8 | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            " " | Out-File -FilePath $PSM1Path -Append
+            " " | Out-File -FilePath $PSM1Path -Append -Encoding utf8
         }
     }
 
     "`n# Functions" | 
-    Out-File -FilePath $PSM1Path -Append
+        Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
     if (Test-Path "$SourcePath/private")
     {
         foreach ($func in (Get-ChildItem -Path "$SourcePath/private" -Filter '*.ps1'))
         {
-            "## $SourceDirectoryName/private/$($func.Name)" | Out-File -FilePath $PSM1Path -Append
+            "## $SourceDirectoryName/private/$($func.Name)" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            Get-Content -Path $func.FullName | Out-File -FilePath $PSM1Path -Append
+            Get-Content -Path $func.FullName -Encoding Utf8 | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            " " | Out-File -FilePath $PSM1Path -Append
+            " " | Out-File -FilePath $PSM1Path -Append -Encoding utf8
         }
     }
 
@@ -64,20 +64,20 @@ task BuildPSD1 -depends 'Init' {
     {
         foreach ($func in (Get-ChildItem -Path "$SourcePath/public" -Filter '*.ps1'))
         {
-            "## $SourceDirectoryName/public/$($func.Name)" | Out-File -FilePath $PSM1Path -Append
+            "## $SourceDirectoryName/public/$($func.Name)" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            Get-Content -Path $func.FullName | Out-File -FilePath $PSM1Path -Append
+            Get-Content -Path $func.FullName -Encoding Utf8 | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-            " " | Out-File -FilePath $PSM1Path -Append
+            " " | Out-File -FilePath $PSM1Path -Append -Encoding utf8
         }
     }
 
     if (Test-Path "$SourcePath/OnModuleLoad.ps1")
     {
-        "`n# Code that runs when module is loaded" | Out-File -FilePath $PSM1Path -Append
+        "`n# Code that runs when module is loaded" | Out-File -FilePath $PSM1Path -Append -Encoding utf8
 
-        Get-Content -Path "$SourcePath/OnModuleLoad.ps1" | Out-File -FilePath $PSM1Path -Append
+        Get-Content -Path "$SourcePath/OnModuleLoad.ps1" -Encoding Utf8 | Out-File -FilePath $PSM1Path -Append -Encoding utf8
     }
 }
 
-task Build -depends 'CopyItems', 'BuildPSD1'
+Task Build -depends 'CopyItems', 'BuildPSD1'
